a757411bc346f37a7f107bc9eac436978ce0b7fd
panikarenko1920-cpu <panikarenko19.20@gmail.com>
2026-01-29 01:59:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,18 @@
 self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('vietnam-trip').then(cache=>{
-    return cache.addAll([
-      './',
-      './index.html',
-      './manifest.json',
-      './sw.js',
-      './style.css'
-    ]);
-  }));
+  e.waitUntil(
+    caches.open('vietnam-trip').then(cache=>{
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './sw.js',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
 });
