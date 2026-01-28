978020ceba9b8bdbaf4a2742cc292e45dd1ff119
panikarenko1920-cpu <panikarenko19.20@gmail.com>
2026-01-29 01:28:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,12 @@
 const CACHE_NAME = 'vietnam-pwa-v1';
-
-const FILES = [
+const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null)))); });
+self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request))); });
